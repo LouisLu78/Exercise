1c4490a4c43d72b14a58e4c63082426fd8863424
Louis <lewgq@yahoo.com>
2019-10-15 21:43:18 +0800

    I am novice for programming, but I am interested in python. It makes me a lot of fun. I hope I can learn a lot in this community(Github)."Hello World！"

--- ex9.py
+++ ex9.py
@@ -1,14 +1,12 @@
 # -*- coding:utf-8 -*-
 import numpy as np
-import pandas as pd
-
 a=range(1,11)
 b=range(10,0,-1)
 
 c1=range(1,13)
 c2=range(11,-1,-1)
 c3=range(12,0,-1)
-ab=list(a)+list(b)
+# ab=list(a)+list(b)
 
 vector=np.array(list(a))
 print(vector)
@@ -24,3 +22,14 @@ h=np.array([list(c1),list(c2),list(c3)])
 print(h)
 print(np.size(h))
 print(h.dtype)
+
+hseg1=h[1:3,2:4]
+print('the small matrix is: ',hseg1)
+
+hseg2=h[[1,1,0],[2,4,5]]
+print(hseg2)
+
+print(h[h>6])
+print(h[1],h[:,0])
+# condition=h>3 and h<9
+# print(condition)
