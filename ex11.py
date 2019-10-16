09ad14ce1278f3225cab302546ca0ac36fb46191
Louis <lewgq@yahoo.com>
2019-10-16 21:25:38 +0800

    I am novice for programming, but I am interested in python. It makes me a lot of fun. I hope I can learn a lot in this community(Github)."Hello World！"

--- ex11.py
+++ ex11.py
@@ -1,42 +1,44 @@
 # -*- coding:utf-8 -*-
+
 import random
 import math
 from ex6 import segment
 
-def randwalk(steps,times):
+def randwalk(steps,times,estimated_ds):
     '''
     The function is to imitate random walk, which starts from the zero point(0,0)
     :param steps: number of steps
     :param times: the number of times for walk
-    :return: the coordinates(x,y) of endpoint of one-time walk with n steps;the distance of one walk;
+    :param estimated_ds: estimated distance for one walk
+    :return: the coordinates(x,y) of endpoint of one-time walk within n steps;the distance of one walk;
     '''
+
     x,y=0,0
     farstep = 0
     nearstep = 0
     dslist = []
     for j in range(times):
         for i in range(steps):
-            direc=random.choice(['E','W','N','S'])
-            if direc=='E' :
-                x+=1
-            elif direc=='W':
-                x-=1
-            elif direc=='N':
-                y+=1
-            else:
-                y-=1
-        d=(x,y)
+            (dx,dy)=random.choice([(1,0),(-1,0),(0,1),(0,-1)])
+            x+=dx
+            y+=dy
+
+        d=[x,y]
         # print(d)
 
         ds = round(math.sqrt(float(d[0]) ** 2 + float(d[1]) ** 2),2)
-        if ds>31:
+        if ds>estimated_ds:
             farstep+=1
         else:
             nearstep+=1
         dslist.append(ds)
+    global dsavg
+
 
     print('The average distance for {} walk is {}'.format(times,sum(dslist)/times))
-    print(segment(dslist,10))
+    # print(segment(dslist,10))
     print(nearstep,farstep)
-randwalk(100,50)
+    dsavg =sum(dslist)/times
+randwalk(100,5000,10)
+randwalk(100,5000,dsavg)
 
