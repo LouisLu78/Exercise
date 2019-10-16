fae9217cdfcbc344656f09fe32e53b22c00816b3
Louis <lewgq@yahoo.com>
2019-10-16 14:44:59 +0800

    I am novice for programming, but I am interested in python. It makes me a lot of fun. I hope I can learn a lot in this community(Github)."Hello World！"

--- ex9.py
+++ ex9.py
@@ -19,17 +19,43 @@ print(np.size(matrix))
 print(matrix.dtype)
 
 h=np.array([list(c1),list(c2),list(c3)])
-print(h)
+print(h,h.shape)
 print(np.size(h))
 print(h.dtype)
 
 hseg1=h[1:3,2:4]
-print('the small matrix is: ',hseg1)
+print(hseg1,hseg1.shape)
+
 
 hseg2=h[[1,1,0],[2,4,5]]
-print(hseg2)
+print(hseg2,hseg2.shape)
 
 print(h[h>6])
 print(h[1],h[:,0])
 # condition=h>3 and h<9
 # print(condition)
+hseg3=h[0:2,6:8]
+print(hseg3,hseg3.shape)
+
+x=np.array(hseg1,dtype=np.float64)
+y=np.array(hseg3,dtype=np.float64)
+# x=np.array(hseg1,dtype=float)
+# y=np.array(hseg3,dtype=float)
+
+print(np.add(x,y))
+print(np.subtract(x,y))
+print(np.multiply(x,y))
+print(np.divide(x,y))
+print(np.sqrt(x))
+print(np.sqrt(y))
+
+
+#today's new course about the dot multiplication of two matrix.20191016
+print(x.dot(y))
+print(np.dot(x,y)) #np.dot
+print(np.sum(x),np.sum(y))
+print(np.sum(y,axis=0),np.sum(y,axis=1))
+# print(h.T)
+# print(x+[1,3]) #broadcasting
+z=np.empty_like(y)
+print(z)
