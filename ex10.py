82f22194b4c5ff4b3878a97800d0fdeb389fadb7
Louis <lewgq@yahoo.com>
2019-10-16 19:12:15 +0800

    I am novice for programming, but I am interested in python. It makes me a lot of fun. I hope I can learn a lot in this community(Github)."Hello World！"

--- ex10.py
+++ ex10.py
@@ -35,41 +35,42 @@ import matplotlib.pyplot as plt
 
 import random
 import math
+from ex6 import segment
 
-def randwalk(n):
+def randwalk(steps,times):
     '''
     The function is to imitate random walk, which starts from the zero point(0,0)
-    :param n: number of steps
-    :return: the coordinates of endpoint of one-time walk with n steps
+    :param steps: number of steps
+    :param times: the number of times for walk
+    :return: the coordinates(x,y) of endpoint of one-time walk with n steps;the distance of one walk;
     '''
-    x=0
-    y=0
-    for i in range(n):
-        step=random.choice(['E','W','N','S'])
-        if step=='E' :
-            x+=1
-        elif step=='W':
-            x-=1
-        elif step=='N':
-            y+=1
+    x,y=0,0
+    farstep = 0
+    nearstep = 0
+    dslist = []
+    for j in range(times):
+        for i in range(steps):
+            direc=random.choice(['E','W','N','S'])
+            if direc=='E' :
+                x+=1
+            elif direc=='W':
+                x-=1
+            elif direc=='N':
+                y+=1
+            else:
+                y-=1
+        d=(x,y)
+        # print(d)
+
+        ds = round(math.sqrt(float(d[0]) ** 2 + float(d[1]) ** 2),2)
+        if ds>31:
+            farstep+=1
         else:
-            y-=1
-    return(x,y)
+            nearstep+=1
+        dslist.append(ds)
 
-farstep=0
-nearstep=0
-k=100
-dslist = []
-for j in range(k):
-    d = randwalk(50)
-    ds = math.sqrt(float(d[0]) ** 2 + float(d[1]) ** 2)
-
-    if ds>=10:
-        farstep+=1
-    else:
-        nearstep+=1
-    dslist.append(ds)
-
-print(dslist)
-print(nearstep,farstep)
+    print('The average distance for {} walk is {}'.format(times,sum(dslist)/times))
+    print(segment(dslist,10))
+    print(nearstep,farstep)
+randwalk(100,50)
 
