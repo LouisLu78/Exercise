672833bb0dfa927a5183e6fda16ba78852f392fa
Louis <lewgq@yahoo.com>
2019-10-29 14:27:36 +0800

    I like python. I am a well-trained chemist with 20 years of expertise, but I am a novice in programming. I hope in the rest of my life, I can grow up as a qualified programmer. "Hello World!"

--- Practice/searchlucky.py
+++ Practice/searchlucky.py
@@ -1,11 +1,43 @@
+# -*- coding:utf8 -*-
 import bs4, webbrowser, sys, requests
 
-print('searching in baidu...')
-res=requests.get('http://www.baidu.com/s?wd=' + ' '.join(sys.argv[1:]))
-res.raise_for_status()
-# print(res.text[:1000])
-soup=bs4.BeautifulSoup(res.text)
-linkElems = soup.select('.r a')
-numOpen = min(5, len(linkElems))
-for i in range(numOpen):
-    webbrowser.open('http://www.baidu.com' + linkElems[i].get('href'))
+# print('searching in baidu...')
+# res=requests.get('http://www.baidu.com/s?wd=' + ' '.join(sys.argv[1:]))
+# res.raise_for_status()
+# # print(res.text[:1000])
+# soup=bs4.BeautifulSoup(res.text)
+# print(soup)
+# linkElems = soup.select('.b a')
+# numOpen = min(5, len(linkElems))
+# for i in range(numOpen):
+#     webbrowser.open('http://www.baidu.com' + linkElems[i].get('href'))
+
+# from selenium import webdriver
+# browser=webdriver.Edge()
+# browser.get('www.python.org')
+
+import docx
+mydoc123=docx.Document("D:\\Arbeiten\\myfiles\\Arbeiten\\学术评论之一百二十三.docx")
+print('Mydoc123 contains {} paragraghs'.format(len(mydoc123.paragraphs)))
+for i in range(len(mydoc123.paragraphs)):
+    print('This is the {} paragragh...'.format(i+1))
+    print(mydoc123.paragraphs[i].text, end='\n')
+# print(mydoc123.paragraphs[1].text)
+# print('Paragragh 1 contains {} runs'.format(len(mydoc123.paragraphs[1].runs)))
+# print(mydoc123.paragraphs[1].runs[1].text)
+#
+# def retrive_text(file):
+#     doctext=docx.Document(file)
+#     fulltext=[]
+#     for para in doctext.paragraphs:
+#         fulltext.append(para.text)
+#     return '\n'.join(fulltext)
+# doc123= retrive_text("D:\\Arbeiten\\myfiles\\Arbeiten\\学术评论之一百二十三.docx")
+# print(doc123)
+# print(len(str(doc123)))
+
+# with open('downfile.txt','r') as f:
+#     cont=f.read()
+# newdoc=docx.Document()
+# newdoc.add_paragraph(cont)
+# newdoc.save('downfile.docx')
