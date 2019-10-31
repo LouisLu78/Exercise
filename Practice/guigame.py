1252049c1651ee778d1f23d36472d1b27700e25a
Louis <lewgq@yahoo.com>
2019-10-31 22:38:35 +0800

    I like python. I am a well-trained chemist with more than 20 years of expertise, but I am a novice in programming. I hope in the rest of my life, I can grow up as a qualified programmer. "Hello World!"

---
+++ Practice/guigame.py
@@ -0,0 +1,21 @@
+# -*- conding:utf8 -*-
+import pyautogui
+from PIL import Image
+
+pyautogui.PAUSE=1
+pyautogui.FAILSAFE=True
+print(pyautogui.size())
+
+# for i in range(10):
+#     pyautogui.moveTo(100, 100, duration=0.25)
+#     pyautogui.moveTo(100, 200, duration=0.25)
+#     pyautogui.moveTo(200, 200, duration=0.25)
+#     pyautogui.moveTo(200, 100, duration=0.25)
+
+print(pyautogui.position())
+pyautogui.scroll(200)
+screen=pyautogui.screenshot()
+
+screen.save('screen.PNG')
+print(pyautogui.locateOnScreen('screen.PNG'))
+print(pyautogui.locateOnScreen('yuna.PNG'))
