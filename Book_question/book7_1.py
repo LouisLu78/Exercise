5fed6eed40e2bb86fef658139b23fb77d3bb1493
Louislu78 <lewgq@yahoo.com>
2019-11-16 21:55:17 +0800

    I believe I have mastered the basic programming skills of this language and I need more practice. As a well-trained chemist with more than 20 years of expertise, I try to apply python in  the field of science.

--- Book_question/book7_1.py
+++ Book_question/book7_1.py
@@ -1,12 +1,28 @@
+'''
+The following programs are the answers to the questions in book <A Primer on Scientific Programming with Python
+Third Edition> by Hans Petter Langtangen. I wrote all the codes myself.
+'''
+
+#Question 1.6
 def interests(p, n):
     '''
     :param p: interest rate a bank is offered.
     :param n: years
     :return:  the total amount of money when the deposit is drawn.
     '''
-    import math
     sum=(1+p/100)**n
     return sum
 A=1000
 A*=interests(5, 5)
 print('If you save 1000 in the bank, after 5 years you may get %.2f back.'%A )
+
+#Question 1.10
+from math import sqrt, exp, pi
+
+def Gaussian_func(m, s, x):
+    p=1/((sqrt(2*pi))*s)
+    i=((x-m)/s)**2
+    return p*exp(-0.5*i)
+m, s, x = 0, 2, 1
+print('The result for the Gaussian function is %.6f' % Gaussian_func(m, s, x))
+
