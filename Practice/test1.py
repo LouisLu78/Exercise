106ca937c083ba49c2f69dcf2a4f69bc17059748
Louislu78 <lewgq@yahoo.com>
2019-11-30 23:16:16 +0800

    I believe I have mastered the basic programming skills of this language and I need more practice. As a well-trained chemist with more than 20 years of expertise, I try to apply python in  the field of science.

---
+++ Practice/test1.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+# author: Guangqiang Lu time:2019/11/30
+# If not explicitly pointed out, all the codes are written by myself.
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+
+# fig, ax=plt.subplots()
+# xdata, ydata=[],[]
+# ln,=ax.plot([],[],'b-',animated=False)
+# def init():
+#     ax.set_xlim(0, 3)
+#     ax.set_ylim(0, 0.6)
+#     return ln,
+#
+# def update(frame):
+#     xdata.append(frame)
+#     ydata.append(frame**2*np.exp(-frame**2))
+#     ln.set_data(xdata, ydata)
+#     return ln,
+#
+# ani = FuncAnimation(fig, update, frames=np.linspace(0, 3, 100),
+#                     init_func=init, blit=True)
+# plt.show()
+#
+# t =np.linspace(0, 3, 51)
+# y1=t**2*np.exp(-t**2)
+# y2=t**2*y1
+# plt.plot(t, y1)
+# plt.plot(t, y2)
+# plt.xlabel('t')
+# plt.ylabel('y')
+# plt.title('Plotting two curves in the same plot')
+# plt.savefig('tmp3.png')
+# plt.show()
+
+fig, ax=plt.subplots()
+xdata, y1data, y2data=[], [], []
+ln,=ax.plot([],[],'b-',animated=False)
+line,=ax.plot([],[],'r-',animated=False)
+def init():
+    ax.set_xlim(0, 3)
+    ax.set_ylim(0, 0.6)
+    return ln, line,
+
+def update(frame):
+    xdata.append(frame)
+    y1data.append(frame**2*np.exp(-frame**2))# two animated curves shown in one figure
+    y2data.append(frame**4*np.exp(-frame**2))
+    ln.set_data(xdata, y1data)
+    line.set_data(xdata, y2data)
+    return ln,line,
+
+ani = FuncAnimation(fig, update, frames=np.linspace(0, 3, 50),
+                    init_func=init, blit=True)
+plt.show()
