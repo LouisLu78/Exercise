7b441a235d99fb0f864d0be90071ca09b4104f83
Louislu78 <lewgq@yahoo.com>
2019-11-30 20:44:14 +0800

    I believe I have mastered the basic programming skills of this language and I need more practice. As a well-trained chemist with more than 20 years of expertise, I try to apply python in  the field of science.

--- Practice/test.py
+++ Practice/test.py
@@ -1,10 +1,10 @@
-# -*- coding: utf-8 -*-
+# -*- coding:utf-8 -*-
 # author: Guangqiang Lu time:2019/11/29
 # If not explicitly pointed out, all the codes are written by myself.
 
 import bs4, requests
-import re, os
-from pdf_convertor import *
+import re, os, shutil
+from pdf_convertor_v2 import *
 
 
 html_file='D:\\Arbeiten\\myfiles\\favorites_2019_11_27.html'
@@ -15,12 +15,19 @@ urls=[]
 count=71
 soup=bs4.BeautifulSoup(f.read(), 'html.parser')
 tags = soup.select("a")
+
 for tag in tags:
 	if regex.search(tag['href']) is not None:
 		urls.append(tag['href'])
-for url in urls[-1:-72:-1]:
-	pdffile= os.path.join(pdffolder, '第%d天.pdf'%count)
-	url_to_pdf(url,pdffile)
+for url in urls[-1:-12:-1]:
+	pdffile= os.path.join(pdffolder, '0%d_.pdf'%count)
+	Convertor().url_to_pdf(url)
+	shutil.move(os.path.join(pdffolder, 'tmp%d.pdf'%Convertor.counter), pdffile)
 	count-=1
 	print('The No.%d file is done'%count)
 f.close()
+
+
+
+
+
