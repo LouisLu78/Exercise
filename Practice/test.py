a676e3157c369eee6dcb85e8275ed5d9c651ac42
Louislu78 <lewgq@yahoo.com>
2019-12-02 22:34:02 +0800

    I believe I have mastered the basic programming skills of this language and I need more practice. As a well-trained chemist with more than 20 years of expertise, I try to apply python in  the field of science.

--- Practice/test.py
+++ Practice/test.py
@@ -7,40 +7,59 @@ import re, os, shutil
 from pdf_convertor_v2 import *
 
 
-html_file='D:\\Arbeiten\\myfiles\\favorites_2019_11_27.html'
-pdffolder = "C:\\Users\\Basanwei\\Downloads\\pdf"
-f=open(html_file,'rb')
-regex=re.compile(r'https://mp\.(.*)')
-urls=[]
-count=71
-soup=bs4.BeautifulSoup(f.read(), 'html.parser')
-tags=soup.select('a')
-for tag in tags:
-	if regex.search(tag['href']) is not None:
-		urls.append(tag['href'])
-for url in urls[-1:-12:-1]:
-	pdffile= os.path.join(pdffolder, '0%d_.pdf'%count)
-	target=Convertor().url_to_pdf(url)
-	shutil.copy(target, pdffile)
-	count-=1
-	print('The No.%d file is downloaded.'%count)
-	os.unlink(target)
-f.close()
+
+# html_file='D:\\Arbeiten\\myfiles\\favorites_2019_11_27.html'
+# pdffolder = "C:\\Users\\Basanwei\\Downloads\\pdf"
+# f=open(html_file,'rb')
+# regex=re.compile(r'https://mp\.(.*)')
+# urls=[]
+# count=71
+# soup=bs4.BeautifulSoup(f.read(), 'html.parser')
+# tags=soup.select('a')
+# for tag in tags:
+# 	if regex.search(tag['href']) is not None:
+# 		urls.append(tag['href'])
+# for url in urls[-1:-12:-1]:
+# 	pdffile= os.path.join(pdffolder, '0%d_.pdf'%count)
+# 	target=Convertor().url_to_pdf(url)
+# 	shutil.copy(target, pdffile)
+# 	count-=1
+# 	print('The No.%d file is downloaded.'%count)
+# 	os.unlink(target)
+# f.close()
 
 
-
-# url = 'https://mp.weixin.qq.com/s?__biz=MzU1NDk2MzQyNg==&mid=2247484049&idx=2&sn=26f28abcdefd030f2fc294f9cca9d140&chksm=fbdada1cccad530a4b619aa4cc2ea75886d02aa0b2d244029e2ac2cffd67e284ea13494c0ed8&scene=21#wechat_redirect'
+# from pdf_convertor import *
+# url = 'https://blog.csdn.net/slhlde/article/details/81937838'
 # headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:70.0) Gecko/20100101 Firefox/70.0'}
 # response = requests.get(url,headers=headers)
+# response.raise_for_status()
+# with open('bs4.html', 'wb') as f:
+#     for chunk in response.iter_content(100000):
+#         f.write(chunk)
+# html_to_pdf('bs4.html',"C:\\Users\\Basanwei\\Downloads\\pdf\\bs4text.pdf")
 # soup=bs4.BeautifulSoup(response.content, 'html.parser')
 # print(soup)
 #
 # tags = soup.select('a')
-#
+# print(tags)
+
 # for tag in tags:
 # 	print(tag['href'])
 
+# from pdf_convertor import *
+# urls =['https://mp.weixin.qq.com/s/OHl_mFfOOv6Ny7lUzK-GLw', 'https://mp.weixin.qq.com/s/1oFf0vXf-vVcSAeBzE-FeA',
+#         'https://mp.weixin.qq.com/s/nEJG5lTawEmyrv5CBKjiOQ', 'https://mp.weixin.qq.com/s/xOmF3JSfGrthhd-qsLQ3oQ',
+#         'https://mp.weixin.qq.com/s/dPqycJjFoajTxLMHoQpC5A']
+# headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:70.0) Gecko/20100101 Firefox/70.0'}
+# count=1
+# for url in urls:
+#     url_to_pdf(url,"C:\\Users\\Basanwei\\Downloads\\pdf\\temp{}.pdf".format(count))
+#     count+=1
+#     print('File no.%d is done.'%count)
 
-
-
-
+html_file='D:\\Arbeiten\\myfiles\\favorites_2019_11_27.html'
+f=open(html_file,'rb')
+soup=bs4.BeautifulSoup(f.read(), 'html.parser')
+for child in soup.dt.next_elements:
+    print(child)
