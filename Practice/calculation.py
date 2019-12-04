bb492914106b4bfa2d911de2028ca951bb84eb47
Louislu78 <lewgq@yahoo.com>
2019-12-04 17:17:10 +0800

    I believe I have mastered the basic programming skills of this language and I need more practice. As a well-trained chemist with more than 20 years of expertise, I try to apply python in  the field of science.

---
+++ Practice/calculation.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# author: Guangqiang Lu time:2019/12/4
+# If not explicitly pointed out, all the codes are written by myself.
+
+from vectors_angle import *
+
+x1=(-1,0,1)
+y1=(1,0,1)
+angle1=angle(x1,y1)
+x2=(0,1,1)
+y2=(1,1,0)
+angle2=angle(x2,y2)
+x3=(3,0,1)
+y3=(0,0,2)
+angle3=angle(x3,y3)
+print('%.2f, %.2f, %.2f'%(angle1, angle2, angle3))
