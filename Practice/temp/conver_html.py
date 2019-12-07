f220d52f98f64fe082848092fd20a1e6d4f8f409
Louislu78 <lewgq@yahoo.com>
2019-12-07 22:01:28 +0800

    I believe I have mastered the basic programming skills of this language and I need more practice. As a well-trained chemist with more than 20 years of expertise, I try to apply python in  the field of science.

---
+++ Practice/temp/conver_html.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# author: Guangqiang Lu time:2019/12/6
+# If not explicitly pointed out, all the codes are written by myself.
+from pdf_convertor import *
+import os, shutil
+
+html=r"C:\Users\Basanwei\Downloads\pyquestions.html"
+# html_to_pdf(html, 'pyquestion.pdf')
+shutil.move('pyquestion.pdf',r"C:\Users\Basanwei\Downloads")
---
+++ Practice/temp/test_map_filter.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# author: Guangqiang Lu time:2019/12/6
+# If not explicitly pointed out, all the codes are written by myself.
+
+m=map(lambda x:x**2, range(10))
+f=filter(lambda x:x%2==1,range(10))
+print(list(m))
+print(list(f))
