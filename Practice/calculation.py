f4a7e092e1082d5f0d19c02b0b5e328a5fac27a0
Louislu78 <lewgq@yahoo.com>
2020-03-20 21:41:53 +0800

    My Python 2020

--- Practice/calculation.py
+++ Practice/calculation.py
@@ -1,21 +1,45 @@
 # -*- coding: utf-8 -*-
 # author: Guangqiang Lu time:2019/12/4
-# If not explicitly pointed out, all the codes are written by myself.
-import sys
-sys.path.append(r'D:\Program files\JetBrains\my_module')
-from vectors_angle import *
+# all the codes below are written by myself.
 
-x1 = (-1,0,1)
-y1 = (1,0,1)
-angle1 = angle(x1,y1)
-x2 = (0,1,1)
-y2 = (1,1,0)
-angle2 = angle(x2,y2)
-x3 = (3,0,1)
-y3 = (0,0,2)
-angle3 = angle(x3,y3)
-print('angle1=%.2f, angle2=%.2f, angle3=%.2f'%(angle1, angle2, angle3))
-x4 = (2,2,1)
-y4 = (0,1,0)
-print('The angle you are calculating is %.2f degree.'%angle(x4,y4))
-print(angle((0,0,1),(2,2,1)))
+def angle(x, y):
+    import numpy as np
+    if len(x) != len(y):
+        raise Exception('The two vectors have unequal numbers of dimensions!' )
+
+    else:
+        X = np.array(x)
+        Y = np.array(y)
+
+        dot = np.dot(X,Y)
+        abs_x = np.sqrt(np.dot(X,X))
+        abs_y = np.sqrt(np.dot(Y,Y))
+        cos_value = dot / (abs_x * abs_y)
+        angle = np.arccos(cos_value)
+        return angle * 180 / np.pi
+
+def main():
+
+    x_axis = [(1,0,0), (0,1,0), (0,0,1), (2,2,1), (1,2,0), (3,1,0)] #Sb2S3 planes
+    y_axis = [(1,0,0), (0,1,0), (0,0,1), (1,0,1), (2,1,1),(3,0,1),(1,3,0)] #Sb2S3 planes
+
+    print('            ', x_axis)
+    for y in y_axis:
+        print(str(y), end="  ")
+        for x in x_axis:
+            print('%9.2f' %angle(y, x), end = "  ")
+        print()
+    print()
+
+    x_axis = [(1,0,0), (0,1,0), (0,0,1), (1,0,1)]#TiO2 planes
+    y_axis = [(1,0,0), (0,1,0), (0,0,1), (1,0,1), (1,1,0), (0,1,1)]#TiO2 planes
+
+    print('            ', x_axis)
+    for y in y_axis:
+        print(str(y), end = "  ")
+        for x in x_axis:
+            print('%9.2f' %angle(y, x), end = "  ")
+        print()
+
+if __name__ == "__main__":
+    main()
